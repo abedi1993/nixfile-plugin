3d17a8f2fd2a78b4edefb510599cfa2851ec7ef1
naserianali <naserianali@gmail.com>
2025-04-09 17:54:20 +0330

    remove test.js

--- assets/js/test.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('#read-more-btn');
-const container = document.querySelector('#read-more');
-container.style.transition = "0.5s";
-btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    container.style.height = "max-content";
-});
